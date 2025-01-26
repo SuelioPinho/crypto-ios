846adedde6c93aa2792ee8dd59ab4c7624d5ec05
Suelio <sueliopss@gmail.com>
2025-01-26 16:22:11 -0300

    First commit

---
+++ CryptoTests/CryptoTests.swift
@@ -0,0 +1,16 @@
+//
+//  CryptoTests.swift
+//  CryptoTests
+//
+//  Created by Suelio Sousa on 26/01/25.
+//
+
+import Testing
+
+struct CryptoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
